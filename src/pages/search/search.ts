c0620375aa4fd21e01c5b21c50a1a748e31ad596
dogansalman <dogan.salman@yahoo.com>
2017-12-04 17:25:25 +0300

    update

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,16 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { Geolocation } from '@ionic-native/geolocation';
-import { Platform } from 'ionic-angular';
-import {
-  GoogleMaps,
-  GoogleMap,
-  GoogleMapsEvent,
-  GoogleMapOptions,
-  CameraPosition,
-  MarkerOptions,
-  Marker
- } from '@ionic-native/google-maps';
 
 @Component({
     selector: 'search',
@@ -18,52 +7,12 @@ import {
 })
 
 export class Search implements OnInit {
-    map: GoogleMap;
 
-    constructor(private googleMaps: GoogleMaps) { }
+    constructor(public navCtrl: NavController) { }
 
-    ionViewDidLoad() {
-      this.loadMap()
-    }
- 
     ngOnInit(): void { }
-   
-    loadMap() {
-          let mapOptions: GoogleMapOptions = {
-            camera: {
-              target: {
-                lat: 43.0741904,
-                lng: -89.3809802
-              },
-              zoom: 18,
-              tilt: 30
-            }
-          };
-      
-          this.map = this.googleMaps.create('map', mapOptions);
-      
-          // Wait the MAP_READY before using any methods.
-          this.map.one(GoogleMapsEvent.MAP_READY)
-            .then(() => {
-              console.log('Map is ready!');
-      
-              // Now you can use all methods safely.
-              this.map.addMarker({
-                  title: 'Ionic',
-                  icon: 'blue',
-                  animation: 'DROP',
-                  position: {
-                    lat: 43.0741904,
-                    lng: -89.3809802
-                  }
-                })
-                .then(marker => {
-                  marker.on(GoogleMapsEvent.MARKER_CLICK)
-                    .subscribe(() => {
-                      alert('clicked');
-                    });
-                });
-      
-            });
-        }
+
+    onInput(e): void { }
+    onCancel(e): void { }
+
 }
