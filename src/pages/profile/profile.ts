7e37fd1e70bf28f718f9ca6ab1abf20313902f0e
dogansalman <dogan.salman@yahoo.com>
2018-01-05 10:44:01 +0300

    #1 profile

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -2,6 +2,7 @@ import { Component} from "@angular/core";
 import { ApiServices } from "../../services/api/api.services";
 import { AuthServices } from "../../services/auth/auth.services";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { ToastServices } from "../../services/toast/toast.services";
 
 @Component({
   selector: 'profile',
@@ -11,7 +12,7 @@ import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 export class Profile  {
   public profileForm: FormGroup;
 
-constructor(private api: ApiServices, private auth: AuthServices, private fb: FormBuilder) {
+constructor(private api: ApiServices, private auth: AuthServices, private fb: FormBuilder, private toast: ToastServices) {
 
   this.profileForm = this.fb.group({
     name: ['', Validators.required],
@@ -23,4 +24,11 @@ constructor(private api: ApiServices, private auth: AuthServices, private fb: Fo
   /* Get and patch user*/
   this.auth.getUser(true).then((user) => this.profileForm.patchValue(user));
 }
+
+  onUpdate(): void {
+    this.api.put('users', this.profileForm.value, {})
+      .then((user) => this.auth.setUser(user))
+      .then(() => this.toast.showToast('Bilgileriniz güncellendi', 2000, 'bottom', false))
+      .catch((err) => this.toast.showToast(err ? err : 'Lütfen tekrar deneyin.',3000, 'bottom'))
+  }
 }
--- src/services/api/api.services.ts
+++ src/services/api/api.services.ts
@@ -32,6 +32,23 @@ export class ApiServices {
         .then(() =>  this.loader.dismissLoading());
     })
   }
+  put(url: string, data: any, header: any): any {
+
+    this.loader.showLoading();
+    return new Promise((resolve, reject) => {
+      this.auth.getToken().then((token) => new Promise((resolve) => resolve( token ? Object.assign({ Authorization: 'Bearer ' + token.access_token }) : header)))
+        .then((header) => this.http.put(env.apiUrl + '/' + url, data, header) )
+        .then((result) => {
+          const _data = () => { try { return JSON.parse(result.data)} catch(e) { return null }};
+          resolve(_data());
+        })
+        .catch((err) => {
+          console.log(err);
+          this.network.type === 'none' ? reject(this.handleError({status: 1})) : reject(this.handleError(err));
+        })
+        .then(() =>  this.loader.dismissLoading());
+    })
+  }
   get(url: string, header: any): any {
     if(this.network.type === 'none') return this.toast.showToast('İnternet bağlantısı bekleniyor...', 2000, 'bottom');
     this.loader.showLoading();
